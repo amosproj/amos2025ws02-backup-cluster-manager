0c8f037765dfe0d285092e5086da982c35dc03a1
Robin Jose Raju <robinrjr10germany@gmail.com>
2025-10-30 18:25:05 +0100

    add: Cluster component
    Signed-off-by: Robin Jose Raju <robinrjr10germany@gmail.com>

---
+++ frontend/src/app/features/clusters/clusters.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clusters',
+  imports: [],
+  templateUrl: './clusters.html',
+  styleUrl: './clusters.css',
+})
+export class Clusters {
+
+}

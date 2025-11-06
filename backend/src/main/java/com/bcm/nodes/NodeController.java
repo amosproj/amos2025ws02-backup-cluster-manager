d37c2c9e367683b34ec5bed3736da84c26b9689f
Robin Jose Raju <robin.jose.raju@fau.de>
2025-11-06 23:25:01 +0100

    add: Node Class, Controller and Service for the  endpoint
    Signed-off-by: Robin Jose Raju <robin.jose.raju@fau.de>

---
+++ backend/src/main/java/com/bcm/nodes/NodeController.java
@@ -0,0 +1,28 @@
+package com.bcm.nodes;
+
+
+import api.model.NodeClass;
+import com.bcm.nodes.NodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class NodeController {
+
+    private final NodeService nodeService;
+
+    @Autowired
+    public NodeController(NodeService nodeService) {
+        this.nodeService = nodeService;
+    }
+
+    @GetMapping("/nodes")
+    public List<NodeClass> getNodes() {
+        return nodeService.getAllNodes();
+    }
+}

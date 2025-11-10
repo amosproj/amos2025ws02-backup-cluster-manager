e7c8bfca4453555d0322b6856c3a7cbfe8aaec9b
Robin Jose Raju <robin.jose.raju@fau.de>
2025-11-10 16:05:57 +0100

    fix: CORS enabled for Frontend using environment properties
    Signed-off-by: Robin Jose Raju <robin.jose.raju@fau.de>

--- backend/src/main/java/com/bcm/nodes/NodeController.java
+++ backend/src/main/java/com/bcm/nodes/NodeController.java
@@ -4,6 +4,7 @@ package com.bcm.nodes;
 import api.model.NodeClass;
 import com.bcm.nodes.NodeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = "${frontend.origin:http://localhost:4200}")
 public class NodeController {
 
     private final NodeService nodeService;

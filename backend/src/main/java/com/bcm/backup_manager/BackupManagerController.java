1f5ec31f7cbad298c79890e726f1e7135125c0c0
Robin Jose Raju <robin.jose.raju@fau.de>
2025-11-11 17:45:30 +0100

    add: /backups endpoint implemented
    Signed-off-by: Robin Jose Raju <robin.jose.raju@fau.de>

--- backend/src/main/java/com/bcm/backup_manager/BackupManagerController.java
+++ backend/src/main/java/com/bcm/backup_manager/BackupManagerController.java
@@ -1,6 +1,10 @@
 package com.bcm.backup_manager;
 
 
+import java.util.List;
+
+import com.bcm.shared.model.BackupDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,5 +16,13 @@ public class BackupManagerController {
     @GetMapping("/backupManager/test")
     public String test(){
         return "This is a backup manager endpoint";
+    }
+     @Autowired
+    private BackupManagerService backupManagerService;
+
+
+    @GetMapping("/backups")
+    public List<BackupDTO> getBackups() {
+        return backupManagerService.getAllBackups();
     }
 }
--- backend/src/main/java/com/bcm/backup_manager/BackupManagerService.java
+++ backend/src/main/java/com/bcm/backup_manager/BackupManagerService.java
@@ -1,8 +1,22 @@
 package com.bcm.backup_manager;
 
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import com.bcm.shared.model.BackupDTO;
 
 @Service
 public class BackupManagerService {
+
+     public List<BackupDTO> getAllBackups() {
+        // Mock data for nodes
+        return Arrays.asList(
+                new BackupDTO(1L, "Backup A", "Active", LocalDateTime.now().minusDays(1)),
+                new BackupDTO(2L, "Backup B", "Inactive", LocalDateTime.now().minusDays(2)),
+                new BackupDTO(3L, "Backup C", "Active", LocalDateTime.now().minusDays(3))
+        );
+    }
 }

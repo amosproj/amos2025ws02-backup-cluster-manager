8579d5a5bec2de4d873e06f0c9e1084c983f8a8e
kruegerhash <landolf.kr2@gmail.com>
2025-11-18 18:38:49 +0100

    test: add missing provideHttpClient import to users test file
    Signed-off-by: kruegerhash <landolf.kr2@gmail.com>

--- frontend/src/app/features/users/users.spec.ts
+++ frontend/src/app/features/users/users.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 import { Users } from './users';
 
@@ -8,7 +9,8 @@ describe('Users', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Users]
+      imports: [Users],
+      providers: [provideHttpClient()]
     })
     .compileComponents();
 

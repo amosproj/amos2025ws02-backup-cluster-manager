bd9f2fa447530317384acfd3c27e07ce8ea959cd
kruegerhash <landolf.kr2@gmail.com>
2025-11-24 11:05:58 +0100

    feat: implement users page by using data tables component, making request to '/users' backend
    Signed-off-by: kruegerhash <landolf.kr2@gmail.com>

--- frontend/src/app/features/users/users.spec.ts
+++ frontend/src/app/features/users/users.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Users } from './users';
+import {provideHttpClient} from '@angular/common/http';
 
 describe('Users', () => {
   let component: Users;
@@ -8,7 +9,8 @@ describe('Users', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Users]
+      imports: [Users],
+      providers: [provideHttpClient()]
     })
     .compileComponents();
 
--- frontend/src/app/features/users/users.ts
+++ frontend/src/app/features/users/users.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {DataTable} from '../../shared/components/data-table/data-table';
+import {UsersService} from './users.service';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [
+    DataTable
+  ],
   templateUrl: './users.html',
   styleUrl: './users.css',
 })
 export class Users {
+  constructor(private usersService: UsersService) {
+  }
 
+  tableColumns = signal([
+    {field: 'id', header: 'ID'},
+    {field: 'name', header: 'Name'},
+    {field: 'enabled', header: 'Enabled'},
+    {field: 'createdAt', header: 'Created At'},
+    {field: 'updatedAt', header: 'Updated At'},
+  ]);
+
+  tableFilters = signal([
+    {
+      label: 'Enabled',
+      active: false,
+    },
+    {
+      label: 'Disabled',
+      active: false,
+    }
+  ]);
+
+  fetchUsers = (page: number, itemsPerPage: number, filters: string, search: string, sortBy: string, sortOrder: string) => {
+    return this.usersService.getUsers(page, itemsPerPage, filters, search, sortBy, sortOrder);
+  }
 }

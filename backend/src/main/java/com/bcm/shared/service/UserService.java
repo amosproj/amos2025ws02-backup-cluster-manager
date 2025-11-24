242845ffe6726eb68cbdc5bb0b32363e92c131ae
kruegerhash <landolf.kr2@gmail.com>
2025-11-24 11:02:09 +0100

    feat: implement user paginating, filtering and searching with db
    Add new DB mappers
    
    Co-authored-by: HansKiller-TH <hanskiller@web.de>
    Signed-off-by: kruegerhash <landolf.kr2@gmail.com>

--- backend/src/main/java/com/bcm/shared/service/UserService.java
+++ backend/src/main/java/com/bcm/shared/service/UserService.java
@@ -1,20 +1,25 @@
 package com.bcm.shared.service;
 
+import com.bcm.shared.filter.Filter;
+import com.bcm.shared.model.api.UserDTO;
 import com.bcm.shared.model.database.User;
 import com.bcm.shared.model.database.UserGroupRelation;
 import com.bcm.shared.repository.UserGroupRelationMapper;
 import com.bcm.shared.repository.UserMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.bcm.shared.pagination.PaginationProvider;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Service for managing user operations.
  * This class provides methods to handle CRUD operations for user data.
  */
 @Service
-public class UserService {
+public class UserService implements PaginationProvider<UserDTO> {
 
     final UserMapper userMapper;
     final UserGroupRelationMapper userGroupRelationMapper;
@@ -42,7 +47,7 @@ public class UserService {
      * @return the user with the specified name, or null if no user is found
      */
     @Transactional
-    public User getUserByName(String name){
+    public User getUserByName(String name) {
         return userMapper.findByName(name);
     }
 
@@ -52,7 +57,7 @@ public class UserService {
      * @return a list of User objects representing all users stored in the repository
      */
     @Transactional
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return userMapper.findAll();
     }
 
@@ -64,7 +69,7 @@ public class UserService {
      * @return the user object representing the newly created user, including the generated ID
      */
     @Transactional
-    public User addUserAndAssignGroup(User user, Long groupID){
+    public User addUserAndAssignGroup(User user, Long groupID) {
         // Groups give users permissions. So a user without a group would be awkward.
         userMapper.insert(user);
         UserGroupRelation userGroupRelation = new UserGroupRelation();
@@ -81,7 +86,7 @@ public class UserService {
      * @return the user object with updated information, retrieved from the database after the update.
      */
     @Transactional
-    public User editUser(User user){
+    public User editUser(User user) {
         userMapper.update(user);
         return userMapper.findById(user.getId());
     }
@@ -93,8 +98,52 @@ public class UserService {
      * @return true if a user was successfully deleted, false otherwise
      */
     @Transactional
-    public boolean deleteUser(Long id){
+    public boolean deleteUser(Long id) {
         // deleting a user should also delete all related user-group relations through cascading
         return userMapper.delete(id) == 1;
+    }
+
+    // Generate example user for testing purposes
+    public void generateExampleUsers(long amount) {
+        for (long i = 1; i <= amount; i++) {
+            User user = new User();
+            user.setName("example_user_" + i);
+            user.setPasswordHash("hashed_password_" + i);
+            user.setEnabled(true);
+            user.setCreatedAt(Instant.now());
+            user.setUpdatedAt(Instant.now());
+            userMapper.insert(user);
+        }
+    }
+
+    @Override
+    public long getTotalItemsCount(Filter filter) {
+        // Add SQL query with filter to get the actual count
+        Boolean isUserEnabled = getUserFilter(filter);
+        return userMapper.getTotalCount(filter.getSearch(),isUserEnabled);
+
+    }
+
+    @Override
+    public List<UserDTO> getDBItems(long page, long itemsPerPage, Filter filter) {
+        // Add SQL query with filter and pagination to get the actual items
+        Boolean isUserEnabled = getUserFilter(filter);
+
+        List<User> users = userMapper.getPaginatedAndFilteredUsers(page, itemsPerPage, filter.getSearch(), filter.getSortBy(), filter.getSortOrder(), isUserEnabled);
+        return users.stream().map(UserDTO::fromUser).toList();
+    }
+
+    private Boolean getUserFilter(Filter filter){
+        Set<String> filters = filter.getFilters();
+        if (filters != null && !filters.isEmpty()) {
+            if (filters.contains("Enabled") && !filters.contains("Disabled")) {
+                // Only enabled users
+                return true;
+            } else if (!filters.contains("Enabled") && filters.contains("Disabled")) {
+                // Only disabled users
+                return false;
+            }
+        }
+        return null;
     }
 }

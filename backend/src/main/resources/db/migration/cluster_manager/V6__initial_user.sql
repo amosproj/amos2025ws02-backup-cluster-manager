f8ed7b86222c5eb95b59fe10718675cffa215527
robinrj6 <32635922+robinrj6@users.noreply.github.com>
2025-12-03 00:04:34 +0100

    Fix SQL syntax by removing quotes from identifiers

--- backend/src/main/resources/db/migration/cluster_manager/V6__initial_user.sql
+++ backend/src/main/resources/db/migration/cluster_manager/V6__initial_user.sql
@@ -1,8 +1,8 @@
-INSERT INTO "users" (name,password_hash,enabled,created_at,updated_at)
+INSERT INTO users (name,password_hash,enabled,created_at,updated_at)
 VALUES ('superuser','$2a$12$XQdirQ8iMErP0f4yISzFSeAZa20QCqwpi3KlQxZgvjWvm0hcI4f0S', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 -- Password is 'su1234'
 
-INSERT INTO "user_group_relations" (user_id,group_id,added_at) 
+INSERT INTO user_group_relations (user_id,group_id,added_at) 
 VALUES ((SELECT id FROM "users" WHERE name='superuser'),
         (SELECT id FROM "groups" WHERE name='Superuser'),
         CURRENT_TIMESTAMP);

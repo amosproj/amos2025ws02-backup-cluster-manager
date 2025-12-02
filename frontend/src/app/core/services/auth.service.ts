742fd7e20ef3713779f55fe3899d853e2fa52efd
kruegerhash <landolf.kr2@gmail.com>
2025-12-02 11:25:02 +0100

    feat(frontend): implement auth services
    Guard - to protect routes, auth service to handle logout, login
    
    Signed-off-by: kruegerhash <landolf.kr2@gmail.com>

---
+++ frontend/src/app/core/services/auth.service.ts
@@ -0,0 +1,87 @@
+import {Injectable, signal} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, of, tap, catchError, map} from 'rxjs';
+import {Router} from '@angular/router';
+import {environment} from '../../../environments/environment';
+import {ApiService} from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private baseUrl = "http://localhost:8080/api/v1/auth";
+  private isAuthenticatedSignal = signal<boolean>(false);
+  private checkingAuth = false;
+
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private apiService: ApiService
+  ) {
+    // Check authentication status on service initialization
+    this.checkAuthStatus();
+  }
+
+  // Public method to get authentication status
+  isAuthenticated(): boolean {
+    return this.isAuthenticatedSignal();
+  }
+
+  // Internal method to check authentication status
+  private checkAuthStatus(): void {
+    if (this.checkingAuth) return;
+    this.checkingAuth = true;
+
+    // TODO: Implement real API call to check session validity
+
+    this.isAuthenticatedSignal.set(true); // demo purpose
+    this.checkingAuth = false;
+  }
+
+  // Login method - Authenticate with backend
+  login(username: string, password: string): Observable<boolean> {
+    return this.http.post(`${this.baseUrl}/login`,
+      {username, password},
+      {observe: 'response'}
+    ).pipe(
+      map(response => {
+        console.log('Login response status:', response.status);
+        console.log('Login response body:', response.body);
+
+        // Status 200 = Erfolg
+        if (response.status === 200) {
+          console.log("Login successful - Status 200");
+          this.isAuthenticatedSignal.set(true);
+          return true;
+        } else {
+          console.log("Login failed - Status:", response.status);
+          this.isAuthenticatedSignal.set(false);
+          return false;
+        }
+      }),
+      catchError(error => {
+        throw error;
+      })
+    );
+  }
+
+  // Logout method - Invalidate session on backend
+  logout(): Observable<boolean> {
+    console.log('Logging out...');
+
+    return this.http.post(`${this.baseUrl}/logout`, {} , {observe: 'response'}
+    ).pipe(
+      map(response => {
+        console.log('Logout response:', response);
+        this.isAuthenticatedSignal.set(false);
+        this.router.navigate(['/login']);
+        return true;
+      }),
+      catchError(error => {
+        console.error('Logout error:', error);
+        this.isAuthenticatedSignal.set(false);
+        throw error;
+      }));
+  };
+}
+

275af4f8d29a484055b26f7d53a7d195be85b1b8
ervinn09 <81773513+ervinn09@users.noreply.github.com>
2026-01-09 15:22:04 +0100

    cmbackupservice migration
    Signed-off-by: ervinn09 <81773513+ervinn09@users.noreply.github.com>

--- backend/src/main/java/com/bcm/shared/pagination/PaginationProvider.java
+++ backend/src/main/java/com/bcm/shared/pagination/PaginationProvider.java
@@ -1,30 +1,31 @@
 package com.bcm.shared.pagination;
 
 import com.bcm.shared.pagination.filter.Filter;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface PaginationProvider<T> {
-    abstract List<T> getDBItems(long page, long itemsPerPage, Filter filter);
-    abstract long getTotalItemsCount(Filter filter);
+    Mono<List<T>> getDBItems(long page, long itemsPerPage, Filter filter);
+     Mono<Long> getTotalItemsCount(Filter filter);
 
-    public default PaginationResponse<T> getPaginatedItems(PaginationRequest paginationRequest){
+    default Mono<PaginationResponse<T>> getPaginatedItems(PaginationRequest paginationRequest) {
         long page = paginationRequest.getPage();
         long itemsPerPage = paginationRequest.getItemsPerPage();
 
         Filter filter = paginationRequest;
 
-        long totalItems = getTotalItemsCount(filter);
+        return getTotalItemsCount(filter)
+                .flatMap(totalItems -> {
+                    long totalPages = (long) Math.ceil((double) totalItems / itemsPerPage);
 
-        List<T> items;
-        long totalPages = (long) Math.ceil((double) totalItems / itemsPerPage);
+                    long safePage = page;
+                    if (safePage > totalPages && totalPages != 0) {
+                        safePage = totalPages;
+                    }
 
-        // Check if requested page exceeds total pages
-        if (page > totalPages && totalPages != 0) {
-            page = totalPages; // Set to last available page
-        }
-        items = getDBItems(page, itemsPerPage, filter);
-
-        return new PaginationResponse<T>(items, page, totalPages, totalItems);
+                    return getDBItems(safePage, itemsPerPage, filter)
+                            .map(items -> new PaginationResponse<>(items, safePage, totalPages, totalItems));
+                });
     }
 }

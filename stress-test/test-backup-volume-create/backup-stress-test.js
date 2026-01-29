d0714afee3256507d26474dfd379f88889d7bb28
Nicolas <nico.troeger@fau.de>
2026-01-29 23:27:15 +0100

    fixed node dtos
    Signed-off-by: Nicolas <nico.troeger@fau.de>

--- stress-test/test-backup-volume-create/backup-stress-test.js
+++ stress-test/test-backup-volume-create/backup-stress-test.js
@@ -7,20 +7,20 @@ const errorRate = new Rate('errors');
 const createBackupDuration = new Trend('backup_create_duration');
 
 const nodes = [
-    '{"id":"132390923902390","name":"cluster-manager:8080","address":"cluster-manager:8080","status":"active","mode":"cluster_manager","isManaged":true}',
-    '{"id":"132390923902390","name":"node1:8081","address":"node1:8081","status":"active","mode":"node","isManaged":true}',
-    '{"id":"132390923902390","name":"node2:8082","address":"node2:8082","status":"active","mode":"node","isManaged":true}',
-    '{"id":"132390923902390","name":"node3:8083","address":"node3:8083","status":"active","mode":"node","isManaged":true}',
-    '{"id":"132390923902390","name":"node4:8084","address":"node4:8084","status":"active","mode":"node","isManaged":true}',
-    '{"id":"132390923902390","name":"node5:8085","address":"node5:8085","status":"active","mode":"node","isManaged":true}',
-    '{"id":"132390923902390","name":"node6:8086","address":"node6:8086","status":"active","mode":"node","isManaged":true}'
+    {"id":"132390923902390","name":"cluster-manager:8080","address":"cluster-manager:8080","status":"active","mode":"cluster_manager","isManaged":true},
+    {"id":"132390923902390","name":"node1-self-register:8081","address":"node1-self-register:8081","status":"active","mode":"node","isManaged":true},
+    {"id":"132390923902390","name":"node2-self-register:8082","address":"node2-self-register:8082","status":"active","mode":"node","isManaged":true},
+    {"id":"132390923902390","name":"node3-self-register:8083","address":"node3-self-register:8083","status":"active","mode":"node","isManaged":true},
+    {"id":"132390923902390","name":"node4-self-register:8084","address":"node4-self-register:8084","status":"active","mode":"node","isManaged":true},
+    {"id":"132390923902390","name":"node5-self-register:8085","address":"node5-self-register:8085","status":"active","mode":"node","isManaged":true},
+    {"id":"132390923902390","name":"node6-self-register:8086","address":"node6-self-register:8086","status":"active","mode":"node","isManaged":true}
 ];
 
 // Test configuration with 5 stages
 export const options = {
     stages: [
         { duration: '5s', target: 1 },        // Stage 1: Warm-up with 1 VU
-        { duration: '20s', target: 300 },   // Stage 2: Ramp up to 100 concurrent users - can't go higher than 100, postgres concurrency limit
+        { duration: '20s', target: 100 },   // Stage 2: Ramp up to 100 concurrent users - can't go higher than 100, postgres concurrency limit
         { duration: '5s', target: 0 },        // Cool-down
     ],
     thresholds: {
